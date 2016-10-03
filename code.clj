f7d718c746df93cf639695c53c9a0b4c0e8fec2c
markbastian <markbastian@gmail.com>
2016-10-03 07:58:32 -0600

    Tweaks

---
+++ code.clj
@@ -0,0 +1,13 @@
+(def bus
+  [{:role :driver :money 4.0}
+   {:role :student
+    :lunch {:type :sack :apples 2 :sandwich 1}}
+   {:role :student
+    :money 4.0}
+   {:role :student
+    :lunch {:type :box :bananas 2 :apples 1 :sandwich 1}
+    :money 5.25}
+   {:role :student :money 10.0}
+   {:role :student
+    :lunch {:type :sack :bananas 1}
+    :money 5.25}])
